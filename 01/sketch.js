2f4294b12a9757a08dad24eccdc71f293d127e8a
francescasarmi <42588201+francescasarmi@users.noreply.github.com>
2018-08-21 15:14:04 -0500

    Add files via upload

---
+++ 01/sketch.js
@@ -0,0 +1,200 @@
+function setup() {
+  createCanvas(600, 600);
+  background(225,226,181);
+}
+function draw() {
+  stroke(231,119,107);
+    fill(238,144,133);
+  ellipse(120,120,225,225);
+  noStroke();
+  fill(217,191,80);
+  ellipse(200,200,100,100);
+  noStroke();
+  fill(189,5,5);
+  ellipse(200,200,80,80);
+  stroke(0,0,0);
+  fill(0,0,0);
+  ellipse(120,120,180,180);
+  stroke(0,0,0);
+  fill(90,54,115);
+  ellipse(120,120,90,90);
+  noStroke();
+  fill(161,160,191);
+  ellipse(120,440,120,120);
+  noStroke();
+  fill(107,142,180);
+  ellipse(120,440,100,100);
+  stroke(0,0,0);
+  fill(217,191,80);
+  ellipse(120,440,80,80);
+  noStroke();
+  fill(226,214,116);
+  ellipse(325,500,80,80);
+  noStroke();
+  fill(219,157,142);
+  ellipse(325,500,60,60);
+  noStroke();
+  fill(34,83,123);
+  ellipse(315,500,55,55);
+  stroke(0,0,0);
+  line(225,575,338,465);
+  stroke(0,0,0);
+  line(200,545,368,430);
+  stroke(0,0,0);
+  line(336,460,400,460);
+  stroke(0,0,0);
+  line(338,480,410,480);
+  stroke(0,0,0);
+  line(500,375,500,600);
+  stroke(0);
+  line(275,400,600,400);
+  noStroke();
+  fill(233,214,216);
+  triangle(500,30,620,400,313,400);
+  noStroke();
+  fill(142,179,205);
+  triangle(150,330,500,360,290,170);
+  stroke(0);
+  noStroke();
+  fill(143,107,47);
+  triangle(600,240,600,325,440,330);
+  line(290,170,150,330);
+  stroke(0);
+  line(290,170,500,360);
+  stroke(160,158,171);
+  fill(255,255,255);
+  arc(402,400,65,60,PI,TWO_PI);
+  stroke(202,177,35);
+  fill(255);
+  arc(468,400,65,60,PI,TWO_PI);
+  stroke(196,44,33);
+  fill(255);
+  arc(535,400,65,60,PI,TWO_PI);
+  stroke(0);
+  line(75,300,420,200);
+  stroke(203,144,148);
+  noFill();
+  arc(440,430,65,60,PI,TWO_PI);
+  stroke(0);
+  noFill();
+  arc(505,430,65,60,PI,TWO_PI);
+  stroke(0);
+  noFill();
+  arc(532,460,65,60,PI,TWO_PI);
+  stroke(0);
+  line(290,60,290,205);
+  stroke(0);
+  line(255,120,310,120);
+  stroke(0);
+  line(265,130,320,130);
+  stroke(0);
+  line(245,70,365,105);
+  stroke(0);
+  line(215,550,500,30);
+  stroke(0);
+  line(500,30,680,620);
+  stroke(0);
+  line(400,50,590,150);
+  stroke(0);
+  line(410,90,590,150);
+  stroke(0);
+  line(330,300,450,190);
+  stroke(0);
+  line(450,190,650,225);
+  stroke(0);
+  line(370,240,650,170);
+  stroke(0);
+  fill(255);
+  ellipse(380,175,60,60);
+  noStroke();
+  fill(0);
+  ellipse(380,175,40,40);
+  stroke(255,0,0);
+  fill(255);
+  ellipse(380,175,20,20);
+  noStroke();
+  fill(174,180,170);
+  ellipse(500,150,30,30);
+  stroke(0);
+  fill(247,241,241);
+  ellipse(500,150,20,20);
+  stroke(0);
+  fill(100);
+  ellipse(570,75,40,40);
+  stroke(255);
+  fill(137,135,157);
+  ellipse(570,75,30,30);
+  noStroke();
+  fill(45,42,129);
+  ellipse(570,75,20,20);
+  stroke(0);
+  fill(100);
+  ellipse(470,50,20,20);
+  stroke(0);
+  fill(255,0,0);
+  ellipse(420,85,15,15);
+  noStroke();
+  fill(239,219,36);
+  triangle(520,40,550,75,490,75);
+  stroke(0);
+  line(290,170,150,330);
+  stroke(0);
+  noFill();
+  arc(400,80,90,150,3.30,4.87);
+  stroke(0);
+  noFill();
+  arc(380,90,45,105,3.30,4.87);
+  stroke(41,35,143);
+  fill(41,35,143);
+  ellipse(580,290,50,50);
+  stroke(41,35,143);
+  fill(234,220,219);
+  ellipse(580,290,40,40);
+  noStroke();
+  fill(0);
+  noStroke();
+  fill(0);
+  ellipse(580,290,10,10);
+  stroke(0);
+  noFill();
+  arc(270,150,105,90,TWO_PI,1.88);
+  noStroke();
+  fill(0);
+  quad(225,280,285,320,275,340,210,310);
+  stroke(0);
+  fill(255);
+  quad(275,340,210,310,200,335,265,360);
+  stroke(0);
+  fill(50,85,27);
+  quad(200,335,265,360,255,380,190,360);
+ 
+  
+  
+  
+  
+  
+  
+  
+ 
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+
+}
